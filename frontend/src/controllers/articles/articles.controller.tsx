65ba4b5de8ad78d2330a541bf9e33e9ff7f2ecdd
Denis Vasilenko <denisvasilenkoise@outlook.com>
2021-07-25 00:09:11 +0500

    feat(article) Написал алгоритм извлечения первого параграфа как описания к статье

--- frontend/src/controllers/articles/articles.controller.tsx
+++ frontend/src/controllers/articles/articles.controller.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { IArticlesController } from "./interfaces";
-import { useRecoilValue } from "recoil";
-import { articlesSelector } from "../../state/articles.state";
+import React, { useEffect } from "react";
+import {IArticlesController} from "./interfaces";
+import {useRecoilValue} from "recoil";
+import {articlesSelector} from "../../state/articles.state";
 
 export const ArticlesController: IArticlesController = (props) => {
 
-  const { View } = props;
+    const {View} = props
 
-  const articles = useRecoilValue(articlesSelector);
+    const articles = useRecoilValue(articlesSelector);
 
-  useEffect(() => {
-    window.onscroll = function() {
-      if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
-        alert("123");
-      }
-    };
+    useEffect(() => {
+        window.onscroll = function() {
+            if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+                alert('123')
+            }
+        };
 
-    return () => {
-      window.onscroll = null;
-    };
-  }, []);
+        return () => {
+            window.onscroll = null;
+        };
+    }, []);
 
-  return <View articles={articles} />;
-};
+    return <View articles={articles}/>
+}
